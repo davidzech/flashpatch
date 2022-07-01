9d5eaab7bc371d9e052eda608a66a363bf42c39b
David Zech <david@davidzech.com>
2022-06-30 18:34:44 -0700

    Get game to boot

--- src/flash/flash.h
+++ src/flash/flash.h
@@ -7,7 +7,7 @@
 #include <gba/flash_internal.h>
 #include <gba/gba.h>
 
-const u16 mxMaxTime[] = {
+constexpr u16 mxMaxTime[] = {
     10,   65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK, 10,   65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK,
     2000, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK, 2000, 65469, TIMER_ENABLE | TIMER_INTR_ENABLE | TIMER_256CLK,
 };
@@ -17,72 +17,143 @@ struct FlashInfo {
     const FlashType type;
 };
 
-static u8 (*PollFlashStatus)(u8 *);
-static u8 sTimerNum;
-static u16 sTimerCount;
-static vu16 *sTimerReg;
-static u16 sSavedIme;
-static u8 sFlashTimeoutFlag = 0;
+// constexpr static u8 (*PollFlashStatus)(u8 *);
+// static u8 sTimerNum;
+// static u16 sTimerCount;
+// static vu16 *sTimerReg;
+// static u16 sSavedIme;
+// static u8 sFlashTimeoutFlag = 0;
 
 template <const FlashInfo &T> u16 WaitForFlashWrite(u8 phase, u8 *addr, u8 lastData);
+u32 VerifyFlashSectorCore(u8 *src, u8 *tgt, u32 size);
+u32 VerifyFlashSector(u16 sectorNum, u8 *src);
+u32 VerifyFlashSectorNBytes(u16 sectorNum, u8 *src, u32 n);
 
 template <const FlashInfo &T> void SwitchFlashBank(u16 sectorNum) {
     // not supported yet
     return;
 }
 
-u8 ReadFlash1(u8 *addr) { return *addr; }
+void InitFlash() {
+    REG_WAITCNT = (REG_WAITCNT & ~WAITCNT_SRAM_MASK) | WAITCNT_SRAM_8;
+    FLASH_WRITE(0x5555, 0xAA);
+    FLASH_WRITE(0x2AAA, 0x55);
+    FLASH_WRITE(0x5555, 0x90);
 
-void SetReadFlash1(u8 *dest) {
-    u16 *src;
-    u16 i;
-
-    PollFlashStatus = decltype(PollFlashStatus)((u8 *)dest + 1);
-
-    src = (u16 *)ReadFlash1;
-    src = (u16 *)((uintptr_t)src ^ 1); // handle thumb
-
-    i = ((u8 *)SetReadFlash1 - (u8 *)ReadFlash1) >> 1;
-
-    while (i != 0) {
-        *dest++ = *src++;
-        i--;
-    }
+    FLASH_WRITE(0x5555, 0xAA);
+    FLASH_WRITE(0x2AAA, 0x55);
+    FLASH_WRITE(0x5555, 0xF0);
 }
 
-void ReadFlashCore(u8 *src, u8 *dest, u32 size) {
+__attribute__((noinline, target("thumb-mode"))) u8 ReadFlashByteCore(u8 *addr) { return *addr; }
+
+__attribute__((noinline)) decltype(&ReadFlashByteCore) SetReadFlashByte(void *dest) {
+    // ldrb r0, [r0]
+    // bx lr
+
+    auto fnDest = (u16 *)dest;
+
+    fnDest[0] = 0x7800;
+    fnDest[1] = 0x4770;
+
+    // u16 *src;
+    // u16 i;
+
+    // u16 *funcSrc;
+    // u16 *funcDest;
+
+    // src = (u16 *)readFlashByteCoreASM;
+    // // src = (u16 *)((uintptr_t)src ^ 1); // remove thumb offset from $PC
+
+    // i = sizeof(readFlashByteCoreASM) / sizeof(*readFlashByteCoreASM);
+    // funcDest = reinterpret_cast<u16 *>(dest);
+
+    // while (i != 0) {
+    //     *funcDest++ = *src++;
+    //     i--;
+    // }
+
+    return decltype(&ReadFlashByteCore)((uintptr_t)fnDest + 1);
+}
+
+template <const FlashInfo &T> __attribute__((noinline)) u16 WaitForFlashWrite(u8 phase, u8 *addr, u8 lastData) {
+    u16 result = 0;
+    u16 delay = 0x2000;
+    u16 buf[0x20];
+    auto readFlashByte = SetReadFlashByte(buf);
+
+    while (readFlashByte(addr) != lastData) {
+        if (delay == 0) {
+            result = 0xA000;
+            break;
+        }
+        delay--;
+    }
+
+    return result;
+}
+
+u8 ReadFlashByte(u8 *addr) {
+    u16 buf[0x20];
+    return SetReadFlashByte(buf)(addr);
+}
+
+__attribute__((noinline, target("thumb-mode"))) void ReadFlashCore(u8 *src, u8 *dest, u32 size) {
     while (size-- != 0) {
         *dest++ = *src++;
     }
 }
 
-void _ReadFlashCoreEND(u16 sectorNum, u32 offset, u8 *dest, u32 size) {}
+// __attribute__((noinline)) void SetReadFlash()
 
 template <const FlashInfo &T> void ReadFlash(u16 sectorNum, u32 offset, u8 *dest, u32 size) {
     u8 *src;
     u16 i;
-    vu16 readFlashCoreBuffer[0x40];
-    vu16 *funcSrc;
-    vu16 *funcDest;
+    /*
+        000000e0  002a       cmp     r2, #0
+        000000e2  05d0       beq     #0xf0
+
+        000000e4  0378       ldrb    r3, [r0]
+        000000e6  0b70       strb    r3, [r1]
+        000000e8  491c       adds    r1, r1, #1
+        000000ea  401c       adds    r0, r0, #1
+        000000ec  521e       subs    r2, r2, #1
+        000000ee  f9d1       bne     #0xe4
+
+        000000f0  7047       bx      lr
+    */
+
+    u16 readFlashCoreBuffer[9];
+    readFlashCoreBuffer[0] = 0x2a00;
+    readFlashCoreBuffer[1] = 0xd005;
+    readFlashCoreBuffer[2] = 0x7803;
+    readFlashCoreBuffer[3] = 0x700b;
+    readFlashCoreBuffer[4] = 0x1c49;
+    readFlashCoreBuffer[5] = 0x1c40;
+    readFlashCoreBuffer[6] = 0x1e52;
+    readFlashCoreBuffer[7] = 0xd1f9;
+    readFlashCoreBuffer[8] = 0x4770;
+
+    u16 *funcSrc;
+    u16 *funcDest;
     decltype(ReadFlashCore) *readFlashCore;
 
     REG_WAITCNT = (REG_WAITCNT & ~WAITCNT_SRAM_MASK) | WAITCNT_SRAM_8;
 
     if (T.type.romSize == FLASH_ROM_SIZE_1M) {
-        SwitchFlashbank<T>(sectorNum / SECTORS_PER_BANK);
+        SwitchFlashBank<T>(sectorNum / SECTORS_PER_BANK);
         sectorNum %= SECTORS_PER_BANK;
     }
 
-    funcSrc = (vu16 *)ReadFlashCore;
-    funcSrc = (vu16 *)((uintptr_t)funcSrc ^ 1);
-    funcDest = readFlashCoreBuffer;
+    // funcSrc = (u16 *)readFlashCoreASM;
+    // funcDest = readFlashCoreBuffer;
 
-    i = ((uintptr_t)_ReadFlashCoreEND - (uintptr_t)ReadFlashCore) >> 1;
+    // i = sizeof(readFlashCoreASM) / sizeof(*readFlashCoreASM);
 
-    while (i != 0) {
-        *funcDest++ = *funcSrc++;
-        i--;
-    }
+    // while (i != 0) {
+    //     *funcDest++ = *funcSrc++;
+    //     i--;
+    // }
 
     readFlashCore = decltype(readFlashCore)((uintptr_t)readFlashCoreBuffer + 1);
 
@@ -91,13 +162,8 @@ template <const FlashInfo &T> void ReadFlash(u16 sectorNum, u32 offset, u8 *dest
     readFlashCore(src, dest, size);
 }
 
-u32 VerifyFlashSectorCore(u8 *src, u8 *tgt, u32 size) { return 0; }
-
-u32 VerifyFlashSector(u16 sectorNum, u8 *src) { return 0; }
-
-u32 VerifyFlashSectorNBytes(u16 sectorNum, u8 *src, u32 n) { return 0; }
-
 template <const FlashInfo &T> static u16 ProgramByte(u8 *src, u8 *dest) {
+    // WAITCNT presumed set by caller
 
     FLASH_WRITE(0x5555, 0xAA);
     FLASH_WRITE(0x2AAA, 0x55);
@@ -132,11 +198,11 @@ try_erase:
     FLASH_WRITE(0x2AAA, 0x55);
     *addr = 0x30;
 
-    SetReadFlash1((u8 *)readFlash1Buffer);
+    // SetReadFlash1((u8 *)readFlash1Buffer);
 
     result = WaitForFlashWrite<T>(2, addr, 0xFF);
 
-    if (!(result & 0xA) || numTries > 3) {
+    if (!(result & 0xA000) || numTries > 3) {
         goto done;
     }
 
@@ -151,7 +217,6 @@ done:
 }
 
 template <const FlashInfo &T> u16 ProgramFlashSector(u16 sectorNum, u8 *src) {
-
     u8 *dest;
     // u16 readFlash1Buffer[0x20];
 
@@ -226,82 +291,45 @@ template <const FlashInfo &T> u32 ProgramFlashSectorAndVerifyNBytes(u16 sectorNu
     return result;
 }
 
-void FlashTimerIntr() {
-    if (sTimerCount != 0 && --sTimerCount == 0) {
-        sFlashTimeoutFlag = 1;
-    }
-}
+// void FlashTimerIntr() {
+//     if (sTimerCount != 0 && --sTimerCount == 0) {
+//         sFlashTimeoutFlag = 1;
+//     }
+// }
 
-u16 SetFlashTimerIntr(u8 timerNum, decltype(FlashTimerIntr) **intrFunc) {
+// u16 SetFlashTimerIntr(u8 timerNum, decltype(FlashTimerIntr) **intrFunc) {
+//     if (timerNum >= 4) {
+//         return 1;
+//     }
 
-    if (timerNum >= 4) {
-        return 1;
-    }
+//     sTimerNum = timerNum;
+//     sTimerReg = (vu16 *)((uintptr_t)REG_ADDR_TMCNT + (timerNum * 4));
+//     *intrFunc = FlashTimerIntr;
+//     return 0;
+// }
 
-    sTimerNum = timerNum;
-    sTimerReg = (vu16 *)((uintptr_t)REG_ADDR_TMCNT + (timerNum * 4));
-    *intrFunc = FlashTimerIntr;
-    return 0;
-}
+// template <const FlashInfo &T> void StartFlashTimer(u8 phase) {
+//     const u16 *maxTimes = &T.maxTime[phase * 3];
+//     sSavedIme = REG_IME;
+//     REG_IME = 0;
+//     sTimerReg[1] = 0;
+//     REG_IE = REG_IE | (INTR_FLAG_TIMER0 << sTimerNum);
+//     sFlashTimeoutFlag = 0;
+//     sTimerCount = *maxTimes++;
+//     *sTimerReg++ = *maxTimes++;
+//     *sTimerReg-- = *maxTimes++;
+//     REG_IF = (INTR_FLAG_TIMER0 << sTimerNum);
+//     REG_IME = 1;
+// }
 
-template <const FlashInfo &T> void StartFlashTimer(u8 phase) {
-    const u16 *maxTimes = &T.maxTime[phase * 3];
-    sSavedIme = REG_IME;
-    REG_IME = 0;
-    sTimerReg[1] = 0;
-    REG_IE = REG_IE | (INTR_FLAG_TIMER0 << sTimerNum);
-    sFlashTimeoutFlag = 0;
-    sTimerCount = *maxTimes++;
-    *sTimerReg++ = *maxTimes++;
-    *sTimerReg-- = *maxTimes++;
-    REG_IF = (INTR_FLAG_TIMER0 << sTimerNum);
-    REG_IME = 1;
-}
-
-void StopFlashTimer() {
-    REG_IME = 0;
-    *sTimerReg++ = 0;
-    *sTimerReg-- = 0;
-}
-
-template <const FlashInfo &T> u16 WaitForFlashWrite(u8 phase, u8 *addr, u8 lastData) {
-    u16 result = 0;
-    u8 status;
-
-    StartFlashTimer<T>(phase);
-
-    while ((status = PollFlashStatus(addr)) != lastData) {
-        if (status & 0x20) {
-            // Write op exceeded time limit)
-
-            if (PollFlashStatus(addr) == lastData) {
-                break;
-            }
-
-            FLASH_WRITE(0x5555, 0xF0);
-            result = phase | 0xA000u;
-            break;
-        }
-
-        if (sFlashTimeoutFlag) {
-            if (PollFlashStatus(addr) == lastData) {
-                break;
-            }
-
-            FLASH_WRITE(0x5555, 0xF0);
-            result = phase | 0xC000u;
-            break;
-        }
-    }
-
-    StopFlashTimer();
-
-    return result;
-}
+// void StopFlashTimer() {
+//     REG_IME = 0;
+//     *sTimerReg++ = 0;
+//     *sTimerReg-- = 0;
+// }
 
 template <const FlashInfo &T> u16 EraseFlashChip() {
     u16 result;
-    u16 readFlash1Buffer[0x20];
 
     REG_WAITCNT = (REG_WAITCNT & ~WAITCNT_SRAM_MASK) | T.type.wait[0];
 
@@ -316,10 +344,15 @@ template <const FlashInfo &T> u16 EraseFlashChip() {
     // erase chip
     FLASH_WRITE(0x5555, 0x10);
 
-    SetReadFlash1((u8 *)readFlash1Buffer); // sets PollFlashStatus... gross code
     result = WaitForFlashWrite<T>(3, FLASH_BASE, 0xFF);
 
     REG_WAITCNT = (REG_WAITCNT & ~WAITCNT_SRAM_MASK) | WAITCNT_SRAM_8;
 
     return result;
 };
+
+u32 VerifyFlashSectorCore(u8 *src, u8 *tgt, u32 size) { return 0; }
+
+u32 VerifyFlashSector(u16 sectorNum, u8 *src) { return 0; }
+
+u32 VerifyFlashSectorNBytes(u16 sectorNum, u8 *src, u32 n) { return 0; }
--- src/sram/sram.h
+++ src/sram/sram.h
@@ -1,80 +1,95 @@
 #include <flash/flash.h>
 
-template <const FlashInfo &T> static constexpr u16 sectorFromAddr(u8 *src) {
+template <const FlashInfo &T> static constexpr inline u16 sectorFromAddr(u8 *src) {
     constexpr auto sectorSize = T.type.sector.size;
 
-    auto offset = src - FLASH_BASE;
-
-    return offset / sectorSize;
+    return (src - FLASH_BASE) / sectorSize;
 }
 
-static void initFlashTimer() {
-    static void (**flashTimerIntrFunc)(void);
+// static void initFlashTimer() {
+//     static void (**flashTimerIntrFunc)(void);
 
-    if (flashTimerIntrFunc == NULL)
-        SetFlashTimerIntr(2, flashTimerIntrFunc);
-}
+//     if (flashTimerIntrFunc == NULL)
+//         SetFlashTimerIntr(2, flashTimerIntrFunc);
+// }
 
 template <const FlashInfo &T> void WriteSRAMUnchecked(u8 *src, u8 *dest, u32 size) {
+    REG_WAITCNT = (REG_WAITCNT & ~WAITCNT_SRAM_MASK) | WAITCNT_SRAM_8;
+    FLASH_WRITE(0x5555, 0xAA);
+    FLASH_WRITE(0x2AAA, 0x55);
+    FLASH_WRITE(0x5555, 0x90);
+
+    FLASH_WRITE(0x5555, 0xAA);
+    FLASH_WRITE(0x2AAA, 0x55);
+    FLASH_WRITE(0x5555, 0xF0);
     constexpr auto sectorSize = T.type.sector.size;
-    initFlashTimer();
     u8 buf[sectorSize];
     u32 bytesLeft = size;
 
-    const auto sectorsToWrite = (size / sectorSize) + 1;
-
     u8 *curSrc = src;
     u8 *curDest = dest;
-    for (int i = 0; i < sectorsToWrite - 1; i++) {
-        ProgramFlashSector<T>(sectorFromAddr<T>(curDest), curSrc);
-        curSrc += sectorSize;
-        curDest += sectorSize;
-        bytesLeft -= sectorSize;
-    }
 
-    // copy dest[:sectorSize] to a temporay buffer since we will have to clobber the whole sector
-    for (int i = 0; i < sizeof(buf); i++) {
-        buf[i] = curDest[i];
-    }
+    // determine sector our address clobbers, and how many bytes we can write to that sector (min(sectorEnd, bytesLeft))
 
-    // copy bytes from src into buf, and then Flash the sector
-    for (int i = 0; i < bytesLeft; i++) {
-        buf[i] = curSrc[i];
+    while (bytesLeft != 0) {
+        u16 sectorNum = sectorFromAddr<T>(curDest);
+        // read entire sector
+        ReadFlash<T>(sectorNum, 0, buf, T.type.sector.size);
+        uintptr_t sectorStart = (uintptr_t)(FLASH_BASE + (sectorNum << T.type.sector.shift));
+        // translate the dest address into buf offset
+        u32 offset = (uintptr_t)curDest - (uintptr_t)sectorStart;
+        u8 *curBuf = buf + offset;
+        // calculate how many bytes we can write to this sector,
+        // at maximum we can only write sectorSize - offset.
+        u32 bytesToWrite = bytesLeft < (sectorSize - offset) ? bytesLeft : (sectorSize - offset);
+        for (u32 i = bytesToWrite; i != 0; i--) {
+            *curBuf++ = *curSrc++;
+        }
+        // Flush buf back into Flash
+        ProgramFlashSector<T>(sectorNum, buf);
+        bytesLeft -= bytesToWrite;
     }
-
-    ProgramFlashSector<T>(sectorFromAddr<T>(curDest), buf);
 }
 
 template <const FlashInfo &T> u8 *WriteSRAMChecked(u8 *src, u8 *dest, u32 size) {
-    constexpr auto sectorSize = T.type.sector.size;
-    initFlashTimer();
-    u8 buf[sectorSize];
-    u16 result;
-    u32 bytesLeft = size;
+    REG_WAITCNT = (REG_WAITCNT & ~WAITCNT_SRAM_MASK) | WAITCNT_SRAM_8;
+    FLASH_WRITE(0x5555, 0xAA);
+    FLASH_WRITE(0x2AAA, 0x55);
+    FLASH_WRITE(0x5555, 0x90);
 
-    const auto sectorsToWrite = (size / sectorSize) + 1;
+    FLASH_WRITE(0x5555, 0xAA);
+    FLASH_WRITE(0x2AAA, 0x55);
+    FLASH_WRITE(0x5555, 0xF0);
+    constexpr auto sectorSize = T.type.sector.size;
+    u8 buf[sectorSize];
+    u32 bytesLeft = size;
 
     u8 *curSrc = src;
     u8 *curDest = dest;
-    for (int i = 0; i < sectorsToWrite - 1; i++) {
-        result = ProgramFlashSectorAndVerify<T>(sectorFromAddr<T>(curDest), curSrc);
-        if (result != 0) {
-            return curDest;
+
+    // determine sector our address clobbers, and how many bytes we can write to that sector (min(sectorEnd, bytesLeft))
+
+    while (bytesLeft != 0) {
+        u16 sectorNum = sectorFromAddr<T>(curDest);
+        // read entire sector
+        ReadFlash<T>(sectorNum, 0, buf, T.type.sector.size);
+        uintptr_t sectorStart = (uintptr_t)(FLASH_BASE + (sectorNum << T.type.sector.shift));
+        // translate the dest address into buf offset
+        u32 offset = (uintptr_t)curDest - (uintptr_t)sectorStart;
+        u8 *curBuf = buf + offset;
+        // calculate how many bytes we can write to this sector,
+        // at maximum we can only write sectorSize - offset.
+        u32 bytesToWrite = bytesLeft < (sectorSize - offset) ? bytesLeft : (sectorSize - offset);
+        for (u32 i = bytesToWrite; i != 0; i--) {
+            *curBuf++ = *curSrc++;
         }
-        curSrc += sectorSize;
-        curDest += sectorSize;
-        bytesLeft -= sectorSize;
+        // Flush buf back into Flash
+        u8 *result = (u8 *)ProgramFlashSectorAndVerify<T>(sectorNum, buf);
+        if (result != 0) {
+            return result;
+        }
+        bytesLeft -= bytesToWrite;
     }
 
-    // copy dest[:sectorSize] to a temporay buffer since we will have to clobber the whole sector
-    for (int i = 0; i < sizeof(buf); i++) {
-        buf[i] = curDest[i];
-    }
-
-    // copy bytes from src into buf, and then Flash the sector
-    for (int i = 0; i < bytesLeft; i++) {
-        buf[i] = curSrc[i];
-    }
-
-    return (ProgramFlashSectorAndVerifyNBytes<T>(sectorFromAddr<T>(curDest), buf, bytesLeft) == 0) ? 0 : curDest;
+    return 0;
 }
