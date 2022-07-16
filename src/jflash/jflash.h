eb1e11f075cf08718688bd398fe2f8bd4fd49c3a
David Zech <david@davidzech.com>
2022-07-15 23:53:45 -0700

    Faster ReadVar and TransferPartition

--- src/jflash/jflash.h
+++ src/jflash/jflash.h
@@ -3,6 +3,7 @@
 #include <common/utils.h>
 #include <flash/flash.h>
 #include <gba/types.h>
+
 namespace JFlash {
 
 enum State : u32 {
@@ -71,10 +72,88 @@ template <const Flash::Info &F, const int EEPROMSize = (8 * 1024)> class Journal
     static_assert(sizeof(Partition::frames) + sizeof(Partition::header) == F.type.romSize / 2);
     static_assert(Partition::numSectors > 0);
 
-    static Partition *Partition0() { return reinterpret_cast<Partition *>(FLASH_BASE); }
-    static Partition *Partition1() { return reinterpret_cast<Partition *>(FLASH_BASE + (F.type.romSize / 2)); }
-    constexpr static s16 *LastFrameIndex() { return (s16 *)0x203fff0; }
+  private:
+    class compactProgress {
+      public:
+        static constexpr int Size = EEPROMSize / 8;
 
+      private:
+        u8 progress[Size];
+        class reference {
+          public:
+            u8 *parent;
+
+            reference(u8 *p) : parent(p) {}
+            reference &operator=(const bool b) {
+                if (b) {
+                    *parent |= 0x80;
+                } else {
+                    *parent &= 0x7F;
+                }
+
+                return *this;
+            }
+
+            operator bool() const { return (*parent & 0x80) == 0x80; }
+        };
+
+      public:
+        void Reset() {
+            for (int i = 0; i < Size; i++) {
+                progress[i] &= 0x7F;
+            }
+        }
+
+        constexpr bool operator[](int index) const { return (progress[index] & 0x80) == 0x80; }
+        reference operator[](int index) { return reference(&progress[index]); }
+    };
+
+    class lookupTable {
+      public:
+        static constexpr int Size = EEPROMSize / 8;
+
+      private:
+        u8 table[Size];
+
+        class reference {
+          public:
+            u8 *parent;
+
+            reference(u8 *p) : parent(p) {}
+            reference &operator=(u8 val) {
+                *parent = (*parent & 0x80) | (val & 0x7F);
+                return *this;
+            }
+
+            operator u8() const { return (*parent & 0x7F); }
+        };
+
+      public:
+        static constexpr int size = EEPROMSize / 8;
+
+        void Reset() {
+            for (int i = 0; i < Size; i++) {
+                table[i] &= 0x80;
+            }
+        }
+
+        constexpr u8 operator[](int index) const { return (table[index] & 0x7F); }
+        reference operator[](int index) { return reference(&table[index]); }
+    };
+
+    struct globals {
+        union {
+            compactProgress CompactProgress;
+            lookupTable LookupTable;
+        };
+        s16 LastFrameIndex;
+    };
+
+    __attribute__((always_inline)) static globals *Globals() { return (globals *)(EWRAM + EWRAM_SIZE - 1) - sizeof(globals); }
+    __attribute__((always_inline)) static Partition *Partition0() { return reinterpret_cast<Partition *>(FLASH_BASE); }
+    __attribute__((always_inline)) static Partition *Partition1() { return reinterpret_cast<Partition *>(FLASH_BASE + (F.type.romSize / 2)); }
+
+  public:
     static void Init() {
         auto activePart = MaybeActivePartition();
         if (activePart == nullptr) {
@@ -88,23 +167,28 @@ template <const Flash::Info &F, const int EEPROMSize = (8 * 1024)> class Journal
         Chip::Write((u8)0x00, (u8 *)&Partition0()->header.active);
     };
 
-    static Maybe<Variable> MaybeReadVar(u16 addr, typename Chip::ReadByteFunc &func, Partition *activePartition = nullptr, bool useLast = true) {
+    static Maybe<Variable> MaybeReadVar(u16 addr, typename Chip::ReadByteFunc &func, Partition *activePartition = nullptr, bool useHint = true) {
         if (activePartition == nullptr) {
             activePartition = ActivePartition();
         }
 
-        s16 *lastAddr = LastFrameIndex();
+        int end = PartitionMaxFrames - 1;
 
-        int end = (useLast && *lastAddr != 0) ? 1 + *lastAddr : PartitionMaxFrames;
+        if (useHint) {
+            int hintAddr = Globals()->LookupTable[addr];
+            if (hintAddr != 0) {
+                end = (hintAddr << 3);
+            }
+        }
 
-        for (int i = end; i > 0; i--) {
+        for (int i = end - 1; i >= 0; i--) {
             Frame *f = &activePartition->frames[i];
             u16 varAddr = Chip::Read(&f->addr, func);
+            if (varAddr != 0xFFFF) {
+                Globals()->LookupTable[varAddr] = (u8)(i >> 3);
+            }
             if (addr == varAddr) {
                 return Chip::Read(&f->data);
-            }
-            if (addr == 0xFFFF) {
-                break;
             }
         }
 
@@ -128,30 +212,33 @@ template <const Flash::Info &F, const int EEPROMSize = (8 * 1024)> class Journal
         return ReadVar(addr, func, activePartition);
     }
 
-    static u16 WriteVar(u16 addr, const Variable &data, Partition *activePartition = nullptr, bool useLast = true) { // Start from lowest address and write
+    static u16 WriteVar(u16 addr, const Variable &data, Partition *activePartition = nullptr, bool useHint = true) { // Start from lowest address and write
         if (activePartition == nullptr) {
             activePartition = ActivePartition();
         }
 
-        s16 *lastAddr = LastFrameIndex();
+        s16 &lastAddr = Globals()->LastFrameIndex;
 
         const Frame newFrame{.addr = addr, .data = data};
         typename Chip::ReadByteFunc func;
 
-        if (!useLast || *lastAddr == 0) {
+        if (!useHint || lastAddr == 0) {
             for (int i = 0; i < PartitionMaxFrames; i++) {
                 Frame *f = &activePartition->frames[i];
                 u16 varAddr = Chip::Read(&f->addr, func);
                 if (varAddr == 0xFFFF) {
+                    if (useHint)
+                        Globals()->LookupTable[addr] = (u8)(i >> 3);
                     Chip::Write(newFrame, f);
-                    *lastAddr = i;
+                    lastAddr = i;
                     return 0;
                 }
             }
-        } else if (useLast && *lastAddr < PartitionMaxFrames - 1) {
-            Frame *f = &activePartition->frames[*lastAddr + 1];
+        } else if (useHint && lastAddr < PartitionMaxFrames - 1) {
+            Frame *f = &activePartition->frames[lastAddr + 1];
+            Globals()->LookupTable[addr] = (u8)((lastAddr + 1) >> 3);
             Chip::Write(newFrame, f);
-            *lastAddr += 1;
+            lastAddr += 1;
             return 0;
         }
 
@@ -173,17 +260,27 @@ template <const Flash::Info &F, const int EEPROMSize = (8 * 1024)> class Journal
         // mark receiving partition as receiving
         Chip::Write((u8)0x00, &receiving->header.receiving);
 
-        *LastFrameIndex() = 0;
-        // transfer latest vars
+        Globals()->LastFrameIndex = 0;
+        auto &compactProgress = Globals()->CompactProgress;
+        compactProgress.Reset();
+
+        // transfer latest vars, by scanning once from the highest address
         typename Chip::ReadByteFunc func;
-        for (int i = 0; i < NumVars; i++) {
+        for (int i = NumVars - 1; i <= 0; i--) {
             if (i == pendingFrame.addr) {
                 continue;
             }
-            // disable lastFrame usage for reading
-            auto maybeVar = MaybeReadVar(i, func, sending, false);
-            if (maybeVar) {
-                auto result = WriteVar(i, *maybeVar, receiving, false);
+
+            Frame *f = &sending->frames[i];
+            u16 varAddr = Chip::Read(&f->addr, func);
+            if (varAddr != 0xFFFF) {
+                // check if we have already hadded this variable
+                if (bool(compactProgress[varAddr])) {
+                    continue;
+                }
+                auto var = Chip::Read(&f->data);
+
+                auto result = WriteVar(varAddr, var, receiving);
                 if (result != 0) {
                     return result;
                 }
@@ -191,7 +288,7 @@ template <const Flash::Info &F, const int EEPROMSize = (8 * 1024)> class Journal
         }
 
         // write pending variable
-        auto result = WriteVar(pendingFrame.addr, pendingFrame.data, receiving, false);
+        auto result = WriteVar(pendingFrame.addr, pendingFrame.data, receiving);
         if (result != 0) {
             return result;
         }
@@ -209,13 +306,11 @@ template <const Flash::Info &F, const int EEPROMSize = (8 * 1024)> class Journal
 
         // dispatch erase command on all sectors of partition but don't wait on sending.
         for (int sector = 0; sector < Partition::numSectors; sector++) {
-            u16 result = Chip::EraseSector(sectorStart + sector, true);
+            u16 result = Chip::EraseSector(sectorStart + sector, false);
             if (result != 0) {
                 return result;
             }
         }
-
-        *LastFrameIndex() = 0;
 
         return result;
     }
